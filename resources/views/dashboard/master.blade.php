3b181101665cbe6c6669779b77d83a8b0321fa5a
Lucas Medina <lucasmedina99.lm@gmail.com>
2020-06-17 08:49:04 -0400

    se soluciona lo anterior agregndo un window.onload en el script de jquery

--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}"></script>
     <title>Modulo Admin</title>
 </head>
 <body>
@@ -14,5 +13,7 @@
         @include('dashboard.partials.session-flash-status')
         @yield('content')
     </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -60,21 +60,23 @@
   </div>
 
   <script>
-    $('#deleteModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget) // Button that triggered the modal
-        var id = button.data('id') // Extract info from data-* attributes
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var action = $('#formDelete').attr('data-action').slice(0,-1)
+    window.onload = function(){
+        $('#deleteModal').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget) // Button that triggered the modal
+            var id = button.data('id') // Extract info from data-* attributes
+            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+            var action = $('#formDelete').attr('data-action').slice(0,-1)
 
-        action = action +  id
+            action = action +  id
 
-        $('#formDelete').attr('action', action);
+            $('#formDelete').attr('action', action);
 
-        var modal = $(this)
-        modal.find('.modal-title').text('Borrar el post con ID: ' + id)
+            var modal = $(this)
+            modal.find('.modal-title').text('Borrar el post con ID: ' + id)
 
-    })
+        })
+    }
   </script>
 @endsection
 
