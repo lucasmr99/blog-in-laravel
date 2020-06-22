6a90431a894a525ea33e2f79bd1ee5ee6c214393
Lucas Medina <lucasmedina99.lm@gmail.com>
2020-06-22 11:02:05 -0400

    se protege crea la funcionalidad para acceder al dashboard (modulo de administracion) se necesie estar logeado

--- app/Http/Controllers/dashboard/CategoryController.php
+++ app/Http/Controllers/dashboard/CategoryController.php
@@ -9,6 +9,11 @@ use App\Http\Requests\StoreCategoryPost;
 
 class CategoryController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/dashboard/PostController.php
+++ app/Http/Controllers/dashboard/PostController.php
@@ -11,6 +11,11 @@ use App\Http\Requests\StorePostPost;
 
 class PostController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
