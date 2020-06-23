92e44e4454e0dbf0cda2408c54a638ee48ac36c5
Lucas Medina <lucasmedina99.lm@gmail.com>
2020-06-23 11:08:31 -0400

    se crea un middewere para que solo el usuario de tipo admin acceda al Dashboar

--- app/Http/Controllers/dashboard/CategoryController.php
+++ app/Http/Controllers/dashboard/CategoryController.php
@@ -13,6 +13,7 @@ class CategoryController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('rol.admin');
     }
     /**
      * Display a listing of the resource.
