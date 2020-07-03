f1ede58b0da88f4ebbf7e9396cedbb0d140ca429
lucas <lucasmedina99.lm@gmail.com>
2020-07-03 15:22:38 -0400

    listdo de categorias en la api /category y listado total category/all

---
+++ app/Http/Controllers/api/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends ApiResponseController
+{
+    public function all(){
+        return $this->successResponse(Category::all());
+    }
+
+    public function index(){
+        return $this->successResponse( Category::Paginate(10) );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,7 @@ Route::resource('post', 'api\PostController')->only([
 ]);
 
 Route::get('post/{category}/category', 'api\PostController@category');
+
+Route::get('category', 'api\CategoryController@index');
+
+Route::get('category/all', 'api\CategoryController@all');
